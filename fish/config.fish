1888bdc4140366b5dc45ae9c9a0c2bec6d66ab8c
Anton <gendra.job@gmail.com>
2024-07-07 18:55:31 +0200

    Remove fish config and add starship config. Change battery waybar style

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,11 @@ end
 set -x JAVA_HOME /usr/share/jvm/jdk-22.0.1
 fish_add_path $JAVA_HOME
 
+set -x STARSHIP_CONFIG ~/.config/starship/starship.toml
+
+function starship_transient_prompt_func
+  starship module character
+end
+starship init fish | source
+enable_transience
+
