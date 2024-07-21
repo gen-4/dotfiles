c3eb9ddb9f7e813a6ec2d70a5abba6710f9c0b50
Anton <gendra.job@gmail.com>
2024-07-21 09:26:36 +0200

    Add lsp servers for rust and c

--- nvim/init.lua
+++ nvim/init.lua
@@ -574,7 +574,8 @@ require('lazy').setup({
         java_language_server = {},
         jdtls = {},
         tsserver = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
+        ast_grep = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
