b706214dda89b23d830d68ce19171b9776858b81
Anton <gendra.job@gmail.com>
2024-09-15 09:15:43 +0200

    Add html and css autocompletion to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -576,6 +576,8 @@ require('lazy').setup({
         tsserver = {},
         rust_analyzer = {},
         ast_grep = {},
+        html = {},
+        cssls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
